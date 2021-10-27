491087c6b7db6467a7c00b3c168228009af98936
Mariam <m.khunjgurua@gmail.com>
2021-10-27 15:28:42 +0400

    add toggle

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,18 +2,29 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: true,
+  };
+
+  onShowClick = (e) => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
 
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} <i className="fas fa-sort-down" />
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
